83a4725960c29fd1e632b37f2f6b31f22cbb7dca
Dmcdominic <dmcdominic@gmail.com>
2019-02-03 18:53:46 -0500

    Setting up a test card

--- src/main/java/slay_by_day/SlayByDay.java
+++ src/main/java/slay_by_day/SlayByDay.java
@@ -1,16 +1,56 @@
 package slay_by_day;
 
+import basemod.BaseMod;
+import basemod.interfaces.EditCardsSubscriber;
+import basemod.interfaces.PostBattleSubscriber;
+import basemod.interfaces.PostDungeonInitializeSubscriber;
+import basemod.interfaces.PostExhaustSubscriber;
+
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+
+import slay_by_day.cards.*;
 
 @SpireInitializer
-public class SlayByDay {
+public class SlayByDay implements PostExhaustSubscriber,
+        PostBattleSubscriber, PostDungeonInitializeSubscriber, EditCardsSubscriber {
+
+    private int count, totalCount;
 
     public SlayByDay() {
-        // TODO: make an awesome mod!
+        BaseMod.subscribe(this);
+        resetCounts();
     }
 
     public static void initialize() {
         new SlayByDay();
     }
 
+    @Override
+    public void receiveEditCards() {
+        BaseMod.addCard(new CartersModTheCard());
+    }
+
+    private void resetCounts() {
+        totalCount = count = 0;
+    }
+
+    @Override
+    public void receivePostExhaust(AbstractCard c) {
+        count++;
+        totalCount++;
+    }
+
+    @Override
+    public void receivePostBattle(AbstractRoom r) {
+        System.out.println(count + " cards were exhausted this battle, " +
+                totalCount + " cards have been exhausted so far this act.");
+        count = 0;
+    }
+
+    @Override
+    public void receivePostDungeonInitialize() {
+        resetCounts();
+    }
 }
---
+++ src/main/java/slay_by_day/cards/CartersModTheCard.java
@@ -0,0 +1,67 @@
+package slay_by_day.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+
+import basemod.abstracts.CustomCard;
+
+
+
+public class CartersModTheCard
+        extends CustomCard {
+    public static final String ID = "SlayByDay:CartersModTheCard";
+    private static CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    // Get object containing the strings that are displayed in the game.
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String IMG_PATH = "img/QoP.png";
+    private static final int COST = 0;
+    private static final int ATTACK_DMG = 3;
+    private static final int UPGRADE_PLUS_DMG = 3;
+    private static final int VULNERABLE_AMT = 1;
+    private static final int UPGRADE_PLUS_VULNERABLE = 1;
+
+    public CartersModTheCard() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                AbstractCard.CardType.ATTACK, AbstractCard.CardColor.RED,
+                AbstractCard.CardRarity.UNCOMMON, AbstractCard.CardTarget.ENEMY);
+        this.magicNumber = this.baseMagicNumber = VULNERABLE_AMT;
+        this.damage=this.baseDamage = ATTACK_DMG;
+
+//        this.setBackgroundTexture("img/custom_background_small.png", "img/custom_background_large.png");
+
+//        this.setOrbTexture("img/custom_orb_small.png", "img/custom_orb_large.png");
+
+//        this.setBannerTexture("img/custom_banner_large.png", "img/custom_banner_large.png");
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new com.megacrit.cardcrawl.actions.common.DamageAction(m,
+                new DamageInfo(p, this.damage, this.damageTypeForTurn),
+                AbstractGameAction.AttackEffect.SLASH_DIAGONAL));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p, new VulnerablePower(m, this.magicNumber, false), this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new CartersModTheCard();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgradeDamage(UPGRADE_PLUS_DMG);
+            this.upgradeMagicNumber(UPGRADE_PLUS_VULNERABLE);
+        }
+    }
+}
