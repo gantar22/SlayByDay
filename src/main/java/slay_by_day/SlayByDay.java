cc0233a4ad728e3909319f2a12c8f70eb2cb8263
Dmcdominic <dmcdominic@gmail.com>
2019-02-03 16:56:11 -0500

    First commit: Initialized the Java project and dependencies.

---
+++ src/main/java/slay_by_day/SlayByDay.java
@@ -0,0 +1,16 @@
+package slay_by_day;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+
+@SpireInitializer
+public class SlayByDay {
+
+    public SlayByDay() {
+        // TODO: make an awesome mod!
+    }
+
+    public static void initialize() {
+        new SlayByDay();
+    }
+
+}
