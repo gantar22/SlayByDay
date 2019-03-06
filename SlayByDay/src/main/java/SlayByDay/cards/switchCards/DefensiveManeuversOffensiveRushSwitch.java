84a817e1e5219e39734b38ae98726deafee1d388
Dmcdominic <dmcdominic@gmail.com>
2019-03-05 20:59:07 -0500

    Drafted Defensive Maneuvers <> Offensive Rush

--- SlayByDay/src/main/java/SlayByDay/cards/switchCards/DefensiveManeuversOffensiveRushSwitch.java
+++ SlayByDay/src/main/java/SlayByDay/cards/switchCards/DefensiveManeuversOffensiveRushSwitch.java
@@ -3,6 +3,7 @@ package SlayByDay.cards.switchCards;
 import SlayByDay.actions.SwitchAction;
 import SlayByDay.characters.TheModal;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -10,23 +11,23 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.NextTurnBlockPower;
 
-import java.util.List;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
-public class PredictionFeatherShieldSwitch extends AbstractSwitchByModeCard {
+public class DefensiveManeuversOffensiveRushSwitch extends AbstractSwitchByModeCard {
 
     public List<switchCard> switchListInherit = Arrays.asList(
-            new AbstractSwitchByModeCard.switchCard("Prediction", "FeatherShield", 1, 0, 0, 15, 5, 0, 0,
+            new AbstractSwitchByModeCard.switchCard("DefensiveManeuvers", "OffensiveRush", 1, 0, 0, 6, 0, 2, 1,
                     CardType.SKILL, CardTarget.SELF, false, false, false, false),
 
-            new AbstractSwitchByModeCard.switchCard("FeatherShield", "Prediction", 1, 0, 0, 9, 3, 0, 0,
-                    CardType.SKILL, CardTarget.SELF, false, false, false, false) );
+            new AbstractSwitchByModeCard.switchCard("OffensiveRush", "DefensiveManeuvers", 1, 3, 0, 0, 0, 4, 1,
+                    CardType.ATTACK, CardTarget.ENEMY, true, false, false, false) );
 
 
-    public PredictionFeatherShieldSwitch(String switchID) {
-        super("SlayByDay:PredictionFeatherShield", "None", null, 0, "None", CardType.SKILL,
-                TheModal.Enums.COLOR_M_PURPLE, CardRarity.COMMON, CardTarget.NONE, PredictionFeatherShieldSwitch.class);
+    public DefensiveManeuversOffensiveRushSwitch(String switchID) {
+        super("SlayByDay:DefensiveManeuversOffensiveRush", "None", null, 0, "None", CardType.SKILL,
+                TheModal.Enums.COLOR_M_PURPLE, CardRarity.BASIC, AbstractCard.CardTarget.NONE, DefensiveManeuversOffensiveRushSwitch.class);
 
         if (switchID == null) {
             switchID = switchListInherit.get(new Random().nextInt(switchListInherit.size())).cardID;
@@ -40,16 +41,17 @@ public class PredictionFeatherShieldSwitch extends AbstractSwitchByModeCard {
         }
     }
 
-    public PredictionFeatherShieldSwitch() { this(null); }
+    public DefensiveManeuversOffensiveRushSwitch() { this(null); }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         switch (this.currentID) {
-            case "Prediction":
-                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new NextTurnBlockPower(p, this.block), this.block));
-                break;
-            case "FeatherShield":
+            case "DefensiveManeuvers":
                 AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, this.block));
+                AbstractDungeon.actionManager.addToBottom(new DrawCardAction(p, this.magicNumber));
+                break;
+            case "OffensiveRush":
+                // Todo - multi damage. Might need custom action, as in Pummel/Whirlwind/FiendFire
                 break;
         }
 
