adabb716016b17fdc3071272eaf561a919101a2e
Joseph Perrino <jperrino@andrew.cmu.edu>
2019-03-24 16:34:35 -0400

    Carter wanted me to commit everything so I did (nothing works)

---
+++ SlayByDay/src/main/java/SlayByDay/cards/switchCards/PossessionExpulsionSwitch.java
@@ -0,0 +1,55 @@
+package SlayByDay.cards.switchCards;
+
+import SlayByDay.actions.SwitchAction;
+import SlayByDay.characters.TheModal;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.NextTurnBlockPower;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.Random;
+
+public class PossessionExpulsionSwitch extends AbstractSwitchByModeCard {
+
+    public List<switchCard> switchListInherit = Arrays.asList(
+            new AbstractSwitchByModeCard.switchCard("Possession", "Expulsion", 1, 0, 0, 0, 0, 0, 0,
+                    CardType.SKILL, CardTarget.SELF, false, false, false, false),
+
+            new AbstractSwitchByModeCard.switchCard("Expulsion", "Possession", 1, 0, 0, 0, 0, 0, 0,
+                    CardType.SKILL, CardTarget.SELF, false, false, false, false) );
+
+    public PossessionExpulsionSwitch(String switchID) {
+        super("SlayByDay:PossessionExpulsionSwitch", "None", null, 0, "None", CardType.SKILL,
+                TheModal.Enums.COLOR_M_PURPLE, CardRarity.BASIC, CardTarget.NONE, PossessionExpulsionSwitch.class);
+
+        if (switchID == null) {
+            switchID = switchListInherit.get(new Random().nextInt(switchListInherit.size())).cardID;
+        }
+
+        this.switchList = switchListInherit;
+        if (this.currentID != null) {
+            this.switchTo(this.currentID);
+        } else {
+            this.switchTo(switchID);
+        }
+    }
+
+    public PossessionExpulsionSwitch() { this(null); }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        switch (this.currentID) {
+            case "Possession":
+                ;
+                break;
+            case "Expulsion":
+                AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, this.block));
+                break;
+        }
+    }
+}
